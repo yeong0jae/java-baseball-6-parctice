bd712d24853ff98b6778cf6167d04712b45eff58
Yeongjae Kim <sees77770@gmail.com>
2024-06-13 16:11:01 +0900

    feat: 컴퓨터의 세자리 수 문자열로 변환
    StringBuilder를 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,9 +18,15 @@ public class Application {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
+        for (int number: computer) {
+            sb.append(number);
+        }
+        String computerString = sb.toString();
+        System.out.println(computerString);
+
+        // 사용자의 세자리 수 입력
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
-
     }
 }
