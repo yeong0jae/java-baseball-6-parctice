31e17b36e0800d17555cf4b1e12e02e25e2367d2
Yeongjae Kim <sees77770@gmail.com>
2024-06-13 16:11:50 +0900

    feat: 게임 종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,5 +28,12 @@ public class Application {
         // 사용자의 세자리 수 입력
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
+        while(true) {
+            String input = Console.readLine();
+            if (input.equals(computerString)) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
     }
 }
