629b0dfdead76630fe66e4e810a2332399d47b1d
Yeongjae Kim <sees77770@gmail.com>
2024-06-24 15:18:49 +0900

    feat: 입력에 대한 볼, 스트라이크 개수 출력 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -33,6 +34,39 @@ public class Application {
             if (input.equals(computerString)) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
+            }
+            int ballCount = 0;
+            int strikeCount = 0;
+            // 첫 번째 수 스트라이크, 볼 판단
+            if (input.charAt(0) == computerString.charAt(0)) {
+                strikeCount += 1;
+            }
+            if (input.charAt(0) == computerString.charAt(1) || input.charAt(0) == computerString.charAt(2)) {
+                ballCount += 1;
+            }
+            // 두 번째 수 스트라이크, 볼 판단
+            if (input.charAt(1) == computerString.charAt(1)) {
+                strikeCount += 1;
+            }
+            if (input.charAt(1) == computerString.charAt(0) || input.charAt(1) == computerString.charAt(2)){
+                ballCount += 1;
+            }
+            // 세 번째 수 스트라이크, 볼 판단
+            if (input.charAt(2) == computerString.charAt(2)) {
+                strikeCount += 1;
+            }
+            if (input.charAt(2) == computerString.charAt(0) || input.charAt(2) == computerString.charAt(1)){
+                ballCount += 1;
+            }
+            // 스트라이크, 볼 출력
+            if (strikeCount == 0 && ballCount == 0) {
+                System.out.println("낫싱");
+            } else if (strikeCount == 0) {
+                System.out.println(ballCount + "볼");
+            } else if (ballCount == 0) {
+                System.out.println(strikeCount + "스트라이크");
+            } else {
+                System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
             }
         }
     }
