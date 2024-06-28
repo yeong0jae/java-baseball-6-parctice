b2987e1a415c9be8320615e23eee0d1ae2322961
Yeongjae Kim <sees77770@gmail.com>
2024-06-28 15:23:06 +0900

    refactor : 메세지 출력 함수 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        Output output = new Output();
+        output.printStartMessage();
 
         String computerString = randomNumber();
         System.out.println(computerString);
@@ -16,7 +17,7 @@ public class Application {
         baseball(computerString);
 
         while (true) {
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            output.printRestartMessage();
             String restartInput = Console.readLine();
             if (restartInput.equals("1")) {
                 computerString = randomNumber();
@@ -25,7 +26,7 @@ public class Application {
                 baseball(computerString);
 
             } else if (restartInput.equals("2")) {
-                System.out.println("게임 종료");
+                output.printEndMessage();
                 break;
             }
         }
