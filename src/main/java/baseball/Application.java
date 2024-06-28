c024d9789f2839f2bdc4ae6c9133b2a642a549fc
Yeongjae Kim <sees77770@gmail.com>
2024-06-29 07:49:35 +0900

    refactor : 불필요한 코드, 주석 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,8 +2,6 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        Output.printStartMessage();
-
         BaseBall baseBall = new BaseBall();
         baseBall.start();
     }
--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -3,16 +3,15 @@ package baseball;
 public class BaseBall {
 
     public void start() {
+        Output.printStartMessage();
 
         RandomBall randomBall = new RandomBall();
         String randomNumber;
 
-        BaseBall baseBall = new BaseBall();
-
         do {
             randomBall.generate();
             randomNumber = randomBall.getRandomNumber();
-            baseBall.play(randomNumber);
+            play(randomNumber);
         } while (Input.isRestart());
     }
 
@@ -25,30 +24,31 @@ public class BaseBall {
                 Output.printEndMessage();
                 break;
             }
+
             int ballCount = 0;
             int strikeCount = 0;
-            // 첫 번째 수 스트라이크, 볼 판단
+
             if (userInput.charAt(0) == randomNumber.charAt(0)) {
                 strikeCount += 1;
             }
             if (userInput.charAt(0) == randomNumber.charAt(1) || userInput.charAt(0) == randomNumber.charAt(2)) {
                 ballCount += 1;
             }
-            // 두 번째 수 스트라이크, 볼 판단
+
             if (userInput.charAt(1) == randomNumber.charAt(1)) {
                 strikeCount += 1;
             }
             if (userInput.charAt(1) == randomNumber.charAt(0) || userInput.charAt(1) == randomNumber.charAt(2)) {
                 ballCount += 1;
             }
-            // 세 번째 수 스트라이크, 볼 판단
+
             if (userInput.charAt(2) == randomNumber.charAt(2)) {
                 strikeCount += 1;
             }
             if (userInput.charAt(2) == randomNumber.charAt(0) || userInput.charAt(2) == randomNumber.charAt(1)) {
                 ballCount += 1;
             }
-            // 스트라이크, 볼 출력
+
             if (strikeCount == 0 && ballCount == 0) {
                 Output.printNothingMessage();
             } else if (strikeCount == 0) {
