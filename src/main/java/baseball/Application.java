522acfdf19f859414fa380ea9c484aace9e6b340
Yeongjae Kim <sees77770@gmail.com>
2024-06-28 16:17:17 +0900

    style : 랜덤 수, 사용자 입력 수 변수명 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,7 +10,7 @@ public class Application {
 
         RandomBall randomBall = new RandomBall();
         String randomNumber = randomBall.generate();
-        
+
         System.out.println(randomNumber);
 
         baseball(randomNumber);
@@ -31,35 +31,35 @@ public class Application {
         }
     }
 
-    private static void baseball(String computerString) {
+    private static void baseball(String randomNumber) {
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
-            String input = Console.readLine();
-            if (input.equals(computerString)) {
+            String userInput = Console.readLine();
+            if (userInput.equals(randomNumber)) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
             int ballCount = 0;
             int strikeCount = 0;
             // 첫 번째 수 스트라이크, 볼 판단
-            if (input.charAt(0) == computerString.charAt(0)) {
+            if (userInput.charAt(0) == randomNumber.charAt(0)) {
                 strikeCount += 1;
             }
-            if (input.charAt(0) == computerString.charAt(1) || input.charAt(0) == computerString.charAt(2)) {
+            if (userInput.charAt(0) == randomNumber.charAt(1) || userInput.charAt(0) == randomNumber.charAt(2)) {
                 ballCount += 1;
             }
             // 두 번째 수 스트라이크, 볼 판단
-            if (input.charAt(1) == computerString.charAt(1)) {
+            if (userInput.charAt(1) == randomNumber.charAt(1)) {
                 strikeCount += 1;
             }
-            if (input.charAt(1) == computerString.charAt(0) || input.charAt(1) == computerString.charAt(2)) {
+            if (userInput.charAt(1) == randomNumber.charAt(0) || userInput.charAt(1) == randomNumber.charAt(2)) {
                 ballCount += 1;
             }
             // 세 번째 수 스트라이크, 볼 판단
-            if (input.charAt(2) == computerString.charAt(2)) {
+            if (userInput.charAt(2) == randomNumber.charAt(2)) {
                 strikeCount += 1;
             }
-            if (input.charAt(2) == computerString.charAt(0) || input.charAt(2) == computerString.charAt(1)) {
+            if (userInput.charAt(2) == randomNumber.charAt(0) || userInput.charAt(2) == randomNumber.charAt(1)) {
                 ballCount += 1;
             }
             // 스트라이크, 볼 출력
