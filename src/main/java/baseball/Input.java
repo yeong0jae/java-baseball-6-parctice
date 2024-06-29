fad5b8cdd7d2a404f2b89f42e642cb119e174b6f
Yeongjae Kim <sees77770@gmail.com>
2024-06-29 11:41:31 +0900

    feat : 사용자가 입력한 숫자 예외 처리

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -6,7 +6,15 @@ public class Input {
 
     public static String readBallNumber() {
         System.out.print("숫자를 입력해주세요 : ");
-        return Console.readLine();
+        String userInput = Console.readLine();
+        validateBallNumber(userInput);
+        return userInput;
+    }
+
+    private static void validateBallNumber(String userInput) {
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static boolean isRestart() {
