049af4b884d145e65630a6e61a8534a4289d0480
Yeongjae Kim <sees77770@gmail.com>
2024-07-06 19:14:15 +0900

    refactor : Input 내의 validate 메소드 삭제

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -1,25 +1,15 @@
 package baseball.view;
 
 import static baseball.utils.Constants.NUMBER_INPUT_MESSAGE;
-import static baseball.utils.Constants.NUMBER_LENGTH;
 import static baseball.utils.Constants.RESTART_INPUT_MESSAGE;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-
     public static String readNumber() {
         System.out.print(NUMBER_INPUT_MESSAGE);
-        String userInput = Console.readLine();
-        validateBallNumber(userInput);
-        return userInput;
-    }
-
-    private static void validateBallNumber(String userInput) {
-        if (userInput.length() != NUMBER_LENGTH) {
-            throw new IllegalArgumentException();
-        }
+        return Console.readLine();
     }
 
     public static boolean isRestart() {
