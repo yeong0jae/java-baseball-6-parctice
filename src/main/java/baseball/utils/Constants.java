6a391e0442991d311a15a88d9d68363bdab3134a
Yeongjae Kim <sees77770@gmail.com>
2024-07-06 23:16:17 +0900

    refactor : 재시작 값 상수로 관리

--- src/main/java/baseball/utils/Constants.java
+++ src/main/java/baseball/utils/Constants.java
@@ -11,4 +11,5 @@ public class Constants {
     public static final int NUMBER_LENGTH = 3;
     public static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String RESTART_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String RESTART_NUMBER = "1";
 }
--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -2,6 +2,7 @@ package baseball.view;
 
 import static baseball.utils.Constants.NUMBER_INPUT_MESSAGE;
 import static baseball.utils.Constants.RESTART_INPUT_MESSAGE;
+import static baseball.utils.Constants.RESTART_NUMBER;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -15,6 +16,6 @@ public class Input {
     public static boolean isRestart() {
         System.out.println(RESTART_INPUT_MESSAGE);
         String userInput = Console.readLine();
-        return userInput.equals("1");
+        return userInput.equals(RESTART_NUMBER);
     }
 }
