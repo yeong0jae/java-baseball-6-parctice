d298e5afa6bee30a908883e1708be175ac42342a
Yeongjae Kim <sees77770@gmail.com>
2024-07-07 17:53:47 +0900

    refactor : 랜덤 볼 생성 시 구현체 주입

--- src/main/java/baseball/model/Balls.java
+++ src/main/java/baseball/model/Balls.java
@@ -2,7 +2,6 @@ package baseball.model;
 
 import static baseball.utils.Constants.NUMBER_LENGTH;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +11,9 @@ public class Balls {
 
     public Balls() {
         List<Integer> balls = new ArrayList<>();
+        NumberGenerator numberGenerator = new NumberGeneratorImpl();
         while (balls.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            int randomNumber = numberGenerator.pickNumberInRange(1, 9);
             if (!balls.contains(randomNumber)) {
                 balls.add(randomNumber);
             }
